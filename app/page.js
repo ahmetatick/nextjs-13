9da85510df2b9295e572fc190ea4e6b5026abd7c
ahmet <mdahmetatik@gmail.com>
2023-05-13 00:28:52 -0400

    v10

--- app/page.js
+++ app/page.js
@@ -1,3 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Link href="/about-us">About Us</Link>
+      <br />
+      <Link href="/docs/post-1">Post 1</Link>
+      <br />
+      <Link href="/docs/post-1/post-2">Post 2</Link>
+      <br />
+      <Link
+        target="_blank"
+        className="test"
+        href={{
+          pathname: "/about-us",
+          query: {
+            name: "Next.js",
+            surname: "ff",
+          },
+        }}
+      >
+        About Us with object
+      </Link>
+    </div>
+  );
 }
